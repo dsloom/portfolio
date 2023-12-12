ff6043f892b7e07f97a889e35a21fda0039268d0
prithvy <prithvykrishna67@gmail.com>
2023-12-12 17:57:39 +0100

    Day 22: Add description, image and url

--- main.py
+++ main.py
@@ -26,13 +26,18 @@ content2 = """
  """
 st.write(content2)
 
-col3,col4 =st.columns(2)
+col3,empty_col,col4 =st.columns([1.5,0.5,1.5])
 
 df = pd.read_csv("/Users/prithvy/pythonProject/app2_portfolio/venv/data.csv",sep=";" )
 with col3:
     for index,row in df[:10].iterrows():
         st.header(row["title"])
-
+        st.write(row["description"])
+        st.image("/Users/prithvy/pythonProject/app2_portfolio/venv/images/"+ row["image"])
+        st.write(f"[sourcecode]({row['url']})")
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("/Users/prithvy/pythonProject/app2_portfolio/venv/images/"+ row["image"])
+        st.write(f"[sourcecode]({row['url']})")
