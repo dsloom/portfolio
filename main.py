871d10b513f2e595c3a92e2077149adc844cd871
prithvy <prithvykrishna67@gmail.com>
2023-12-12 17:37:21 +0100

    Day 22: Apply the titles in the column

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
+import pandas as pd
 
-st.set_page_config(layout = "wide")
+st.set_page_config(layout ="wide")
 col1,col2 = st.columns(2)
 
 
@@ -20,6 +21,18 @@ with col2:
     """
     st.info(content)
 content2 = """
+
  Below you can find some of the apps I have built in Python. Feel free to contact me!
  """
 st.write(content2)
+
+col3,col4 =st.columns(2)
+
+df = pd.read_csv("/Users/prithvy/pythonProject/app2_portfolio/venv/data.csv",sep=";" )
+with col3:
+    for index,row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
